e8bab1d136e74ab97afbbd7718db173c9000ade6
sanjeevanand <sanjeevanand32@gmail.com>
2026-08-08 21:36:26 +0530

    fix test mongodb connection

--- src/db.js
+++ src/db.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/student_db");
+    // await mongoose.connect("mongodb://localhost:27017/student_db");
+    await mongoose.connect("mongodb://host.docker.internal:27017/student_db_test");
 
     console.log("MongoDB connected");
   } catch (error) {
